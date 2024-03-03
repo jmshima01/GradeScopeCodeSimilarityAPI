6131b0eb196f3b19c2d9d12c647b6d364bd12292
jmshima01 <jamesshima@mines.edu>
2024-03-03 05:34:45 -0700

    good start gold mine basically done

--- main.py
+++ main.py
@@ -15,23 +15,68 @@ if __name__ == "__main__":
     name = "First & Last Name Swap"
     print(d.columns)
     
-        
     students = {name[0]:{} for name in d.loc[:,[name]].values}
  
     fix_percent = lambda x : list(map(lambda y: int(y[:-1]),x))
-    
+    fix_sec = lambda x : list(map(lambda y: y[-1] if type(y)==str else y,x))
     d["% Similarity"] = fix_percent(d["% Similarity"])
-    
+    d["Sections"] = fix_sec(d["Sections"])
     data = d.loc[(d["File"] == "submission.py") & (d["% Similarity"] >= cutoff)].sort_values("% Similarity",ascending=False)
-    for i,n in enumerate(data[name].values):
+    data = data.drop(columns=['Last, First Name Swap','Unnamed: 7'])
+    data = data.rename(columns={'First & Last Name Swap':'Name','Sections':'Section','% Similarity':'Similarity'})
+
+    for i,n in enumerate(data['Name'].values):
         students[n]["Top Source"]=data["Top Source"].values[i]
-        students[n]["% Similarity"]=data["% Similarity"].values[i]
+        students[n]["Similarity"]=data["Similarity"].values[i]
+        students[n]["Section"]=data["Section"].values[i]
+        
     
+    blame= {}
     for i,v in students.items():
         if v:
-            print(f"{i}:{v}")
+            blame[i]=v
+    for i,v in blame.items():
 
+        print(i,":",v)
+    
+    data.to_csv("basic.csv",index=False)
+
+    groups = set()
+    
+    for k in blame:
+        group = set()
+        
+        group.add(k)
+        for i,v in blame.items():
+            if i == k:
+                group.add(v["Top Source"])
+            if v["Top Source"] == k:
+                group.add(i)
+        group = tuple(sorted(list(group)))
+        if len(group) > 2:
+            groups.add(group)
+
+    groups = list(groups)
+    print()
+    print(groups)
+
+    group_avg = {}
+    for k in groups:
+        avg = 0
+        l = len(k)
+        for i in k:
+            if i not in blame.keys():
+                print(i," Not in blame")
+                l-=1
+                continue
+            avg+=blame[i]['Similarity']
+        group_avg[k] = round(float(avg)/float(l),2)
+    
+    for i,v in group_avg.items():
+        print(i,":",v)
+
+        
 
     
 
-    
+        
